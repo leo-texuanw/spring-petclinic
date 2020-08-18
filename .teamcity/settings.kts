6266ffb37d98071b177b95a0e22d6ded740a39bc
leo.wu <leo.wu@TeamCity>
2020-08-18 21:08:57 +1000

    TeamCity change in 'Spring Petclinic' project: 'Build_DSL' build configuration was removed

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,6 +1,4 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
-import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -27,26 +25,4 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2020.1"
 
 project {
-
-    buildType(BuildDsl)
 }
-
-object BuildDsl : BuildType({
-    name = "Build_DSL"
-
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    steps {
-        maven {
-            goals = "clean test"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
-        }
-    }
-
-    triggers {
-        vcs {
-        }
-    }
-})
